1373cb0c32698512e28da1e25f9ced0b46a85c63
Lauren Dorval <dorvallg@dukes.jmu.edu>
2020-12-08 14:47:15 -0500

    change store

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
+
 import store from './store'
 import {Provider} from 'react-redux';
-=======
-import { Provider } from 'react-redux';
-import store from './store';
->>>>>>> c5c7ecebb94f0c5018574d0190f6c05ca74a8db5
 
 ReactDOM.render(
   
